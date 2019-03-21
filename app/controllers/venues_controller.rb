3a6a72897b77184e6fe1f618b6bd1d0e64bf371a
Zach Wiener <zwiener0@chicagobooth.edu>
2019-03-21 17:02:16 +0000

    Working filters now

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -10,8 +10,8 @@ class VenuesController < ApplicationController
     
   def index
     @venue_style=true
-    @q = Venue.ransack(params.fetch("q", nil))
-    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).per(10)
+    @q = current_user.bookmarked_venues.ransack(params.fetch("q", nil))
+    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).all
 
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
